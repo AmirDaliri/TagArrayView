64a4a63fe6ce3245d4176aa27eaeca4be3ae4813
idaliri <idaliri69@gmail.com>
2018-07-06 13:37:14 +0430

    initial commit

---
+++ TagArrayView/VC/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  TagArrayView
+//
+//  Created by Amir Daliri on 4/15/1397 AP.
+//  Copyright © 1397 AmirDaliri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController , TagListViewDelegate {
+    
+    @IBOutlet weak var tagListView: TagListView!
+    @IBOutlet weak var biggerTagListView: TagListView!
+    @IBOutlet weak var biggestTagListView: TagListView!
+    
+    // MARK: - Lifecycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // I'm Here...
+        setupView()
+    }
+    
+    // MARK: - Tag List Setup View
+    
+    private func setupView() {
+        tagListView.delegate = self
+        tagListView.addTag("TagListView")
+        tagListView.addTag("TEAChart")
+        tagListView.addTag("To Be Removed")
+        tagListView.addTag("To Be Removed")
+        tagListView.addTag("Quark Shell")
+        tagListView.removeTag("To Be Removed")
+        tagListView.addTag("On tap will be removed").onTap = { [weak self] tagView in
+            self?.tagListView.removeTagView(tagView)
+        }
+        
+        let tagView = tagListView.addTag("gray")
+        tagView.tagBackgroundColor = UIColor.gray
+        tagView.onTap = { tagView in
+            print("Don’t tap me!")
+        }
+        
+        tagListView.insertTag("This should be the third tag", at: 2)
+        
+        biggerTagListView.delegate = self
+        biggerTagListView.textFont = UIFont.systemFont(ofSize: 15)
+        biggerTagListView.shadowRadius = 2
+        biggerTagListView.shadowOpacity = 0.4
+        biggerTagListView.shadowColor = UIColor.black
+        biggerTagListView.shadowOffset = CGSize(width: 1, height: 1)
+        biggerTagListView.addTag("Inboard")
+        biggerTagListView.addTag("Pomotodo")
+        biggerTagListView.addTag("Halo Word")
+        biggerTagListView.alignment = .center
+        
+        biggestTagListView.delegate = self
+        biggestTagListView.textFont = UIFont.systemFont(ofSize: 24)
+        // it is also possible to add all tags in one go
+        biggestTagListView.addTags(["all", "your", "tag", "are", "belong", "to", "us"])
+        biggestTagListView.alignment = .right
+    }
+    
+    // MARK: - Tag List ViewD elegate
+    
+    func tagPressed(_ title: String, tagView: TagView, sender: TagListView) {
+        print("Tag pressed: \(title), \(sender)")
+        tagView.isSelected = !tagView.isSelected
+    }
+    
+    func tagRemoveButtonPressed(_ title: String, tagView: TagView, sender: TagListView) {
+        print("Tag Remove pressed: \(title), \(sender)")
+        sender.removeTagView(tagView)
+    }
+}
+
